dbb8fc6df25b39b30e24c3e7bd751e1eaa7a12c8
Abdelrhman Osama <abdelrhmanosama1110@gmail.com>
2025-08-24 08:42:55 +0300

    fix form size on sm screens

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -53,7 +53,7 @@ export default function SignUp() {
       {apiErrorResp?(<div className="p-4 mb-5 text-xl font-bold text-center text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
          {apiErrorResp}
       </div>): null}
-      <form onSubmit={handleSubmit(handleRegister)} className="max-w-md mx-auto my-5">
+      <form onSubmit={handleSubmit(handleRegister)} className="md:max-w-md w-[70%] mx-auto my-5">
         <div className="relative z-0 w-full mb-5 group">
           <input type="text" {...register('name')} id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
           <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">name</label>
