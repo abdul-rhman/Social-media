9b0ea693d1d5419f9b022eb21888dae5ad4aaa50
Abdelrhman Osama <abdelrhmanosama1110@gmail.com>
2025-08-24 08:30:56 +0300

    fix login css importing bug

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import style from './Login.module.css'
+import style from './LogIn.module.css'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import z from 'zod'
