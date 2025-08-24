fda5aa196bd75e03660d1f2a72bf07ed917987ab
Abdelrhman Osama <abdelrhmanosama1110@gmail.com>
2025-08-24 09:29:11 +0300

    fix navbar drop list

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import style from './Nav.module.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { userContext } from './../../Contexts/UserContext';
@@ -11,12 +11,33 @@ export default function Nav() {
   let{token,changeToken,setuserId,id}=useContext(userContext);
   let queryClient = useQueryClient();
   const navigate = useNavigate();
+  let [showNavDropDown,setShowNavDropDown] = useState(false);
+  let navDropDownRef = useRef(null);
   
   let {error,isError,data,isLoading} = useQuery({
     queryKey:['currentData'],
     queryFn:()=>axios.get('https://linked-posts.routemisr.com/users/profile-data',{headers:{token}}),
     select:(data)=>data.data.user
   })
+
+useEffect(() => {
+    function handleClickOutside(e) {
+      if (navDropDownRef.current && !navDropDownRef.current.contains(e.target)) {
+        setShowNavDropDown(false);
+      }
+    }
+
+    if (showNavDropDown) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [showNavDropDown]);
+
   function handleLogout(){
     changeToken(null);
     setuserId(null);
@@ -36,14 +57,13 @@ export default function Nav() {
         </Link>
         <div className="flex gap-3">
         {token?(<>
-          <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex items-center relative md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           
-            <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-              <span className="sr-only">Open user menu</span>
+            <button ref={navDropDownRef} type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" onClick={(e) => { setShowNavDropDown((prev) => !prev); e.stopPropagation(); }} >
               <img className="w-8 h-8 rounded-full" src={data?.photo} alt="user photo" />
             </button>
             {/* Dropdown menu */}
-            <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+            {showNavDropDown && <div className="z-50  absolute top-8  right-0 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" >
               <div className="px-4 py-3">
                 <span className="block text-sm text-gray-900 dark:text-white">{data?.name}</span>
                 <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{data?.email}</span>
@@ -59,7 +79,7 @@ export default function Nav() {
                   <span onClick={handleLogout}  className="cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</span>
                 </li>
               </ul>
-            </div>
+            </div>}
           </div>
           </>):
           (<>
