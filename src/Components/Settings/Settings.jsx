2e90cd5299429a6f974f9145535a6d04f1859ae0
Abdelrhman Osama <abdelrhmanosama1110@gmail.com>
2025-08-24 14:06:31 +0300

    fix settings page view in small screens

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -60,7 +60,7 @@ export default function Settings() {
 
   return (
     <div className='w-full md:w-[80%] mx-auto border-2 border-slate-800 rounded-lg p-4 mt-6 bg-sky-100'>
-        <div className="flex flex-wrap h-[80vh]">
+        <div className="flex flex-wrap h-full my-2 lg:h-[80vh]">
           <div className='md:w-1/2 w-full flex  justify-center items-center '>
             <div className='w-fit h-fit relative'>
               <label htmlFor="change-img">
@@ -73,7 +73,7 @@ export default function Settings() {
             </div>
           </div>
           <div className='md:w-1/2 w-full flex  justify-center items-center'>
-            <form onSubmit={handleSubmit(handleChangePassword)} className="mx-auto my-5 lg:w-[70%] ">
+            <form onSubmit={handleSubmit(handleChangePassword)} className="mx-auto my-5 w-[90%] md:w-[70%] ">
               <div className="relative z-0 w-full mb-10 group">
                 <input type="password" {...register('password')} id="password" className=" block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                 <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Current Password</label>
