5115a6175365f1a877936a12311f57f452c0e941
Abdelrhman Osama <abdelrhmanosama1110@gmail.com>
2025-08-24 08:59:50 +0300

    fix responsivness on edit post and force the l-mode

--- src/Components/EditPostDialog/EditPostDialog.jsx
+++ src/Components/EditPostDialog/EditPostDialog.jsx
@@ -94,9 +94,9 @@ export default function EditPostDialog({post ,closeDialog}) {
                   <i className="fa-solid fa-xmark size-[20px] text-slate-500 cursor-pointer" onClick={()=>setImage(prevImage.current)}></i></p>
                 </>}
               </div>
-              <div className='ms-auto'>
+              <div className='ms-auto gap-1 flex md:flex-row flex-col'>
                 <button type="submit" disabled={isLoading}  className=" cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{isLoading?<i className='fas fa-spinner fa-spin'></i>:'Edit Post'}</button>
-                <button type="button" disabled={isLoading}  className="ms-2 cursor-pointer text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800" onClick={closeDialog}>cancel</button>
+                <button type="button" disabled={isLoading}  className="cursor-pointer text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800" onClick={closeDialog}>cancel</button>
               </div>
             </div>
           </form>
